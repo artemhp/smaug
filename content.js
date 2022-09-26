479bbf896f8a4c5b56495aab0b45d0ecb3206a3f
artemhp <artemhp@gmail.com>
2022-09-26 12:14:53 -0400

    Fix army frame

--- content.js
+++ content.js
@@ -1,203 +1,265 @@
 setTimeout(function () {
-    (function loop() {
-        var rand = Math.random() * (2000 - 1000) + 1000;
-        setTimeout(function () {
+  (function loop() {
+    var rand = Math.random() * (2000 - 1000) + 1000;
+    setTimeout(function () {
+      if (
+        getImageOfLocation(getTravelFrame()) ==
+        "http://fantasyland.ru/images/places/Global/arena.jpg"
+      ) {
+        smaugGet("statistics", function (a) {
+          var dateFormat = smaugDateFormat();
+          if (getExpNode()) {
+            a.statistics["daily"][dateFormat]["experience"] =
+              getExpNode().innerHTML.match(/\d+$/)[0];
+            smaugSet(
+              {
+                statistics: a.statistics,
+              },
+              function () {}
+            );
+          }
+        });
 
-            if (getImageOfLocation(getTravelFrame()) == "http://fantasyland.ru/images/places/Global/arena.jpg") {
+        if (getInputCaptcha(getTravelFrame())) {
+          chrome.runtime.sendMessage(
+            { action: "captcha" },
+            function (response) {
+              getInputCaptcha(getTravelFrame()).focus();
+            }
+          );
+        }
+      }
 
-                smaugGet('statistics', function (a) {
-                    var dateFormat = smaugDateFormat();
-                    if ( getExpNode()) {
-                        a.statistics["daily"][dateFormat]["experience"] = getExpNode().innerHTML.match(/\d+$/)[0];
-                        smaugSet({
-                            'statistics': a.statistics
-                        }, function () {});
-                    }
-                });
+      if (
+        getTravelFrame() &&
+        getImageOfLocation(getTravelFrame()) !==
+          "http://fantasyland.ru/images/places/Global/arena.jpg"
+      ) {
+        //chrome.storage.local.clear();
+        chrome.runtime.sendMessage({ action: "travel" });
 
-                if (getInputCaptcha(getTravelFrame())) {
-                    chrome.runtime.sendMessage({action: "captcha"}, function(response) {
-                        getInputCaptcha(getTravelFrame()).focus()
-                    });
+        smaugGet("action", function (act) {
+          if (act.action == "go") {
+            smaugGet(["coordinates", "storedCoordinatesTraps"], function (a) {
+              if (!a.coordinates) {
+                storedCoordinates = [];
+              } else {
+                storedCoordinates = a.coordinates;
+              }
+
+              // Try to avoid this
+              storedCoordinatesArray = storedCoordinates.map(function (el) {
+                if (el && el.local) {
+                  return el.local;
                 }
+              });
+
+              storedCoordinatesTrapArray = a.storedCoordinatesTraps;
+
+              ////////// Action
+              actionOnTravelFrame();
+            });
+          }
+        });
+      }
+      loop();
+    }, rand);
+  })();
+
+  (function loop() {
+    var rand = Math.random() * (18000 - 10000) + 10000;
+    setTimeout(function () {
+      if (getArmyFrame()) {
+        smaugGet("action", function (act) {
+          if (act.action == "go") {
+            var randArmy = Math.floor(Math.random() * 3);
+            var unit = getUnitNode(getArmyFrame(), locale.army[randArmy]);
+
+            if (unit) {
+              if (getArmyFrame()) {
+                unit.click();
+              }
+              setTimeout(function () {
+                if (getArmyFrame()) {
+                  getArmyFrame().location.reload();
+                  getCombatField().location.reload();
+                }
+              }, 3000);
             }
 
-            if (getTravelFrame() && getImageOfLocation(getTravelFrame()) !== "http://fantasyland.ru/images/places/Global/arena.jpg") {
+            if (getHealthNode(getArmyFrame())) {
+              var healthCheck = parseInt(
+                getHealthNode(getArmyFrame())
+                  .getAttribute("style")
+                  .match(/\d+/)[0]
+              );
+              chrome.runtime.sendMessage({
+                action: "combat",
+                health: healthCheck,
+              });
 
-                //chrome.storage.local.clear();
-                chrome.runtime.sendMessage({action: "travel"});
-
-                smaugGet('action', function (act) {
-                    if (act.action == "go") {
-                        smaugGet(['coordinates', 'storedCoordinatesTraps'], function (a) {
-
-                            if (!a.coordinates) {
-                                storedCoordinates = [];
-                            } else {
-                                storedCoordinates = a.coordinates;
-                            }
-
-                            // Try to avoid this
-                            storedCoordinatesArray = storedCoordinates.map(function (el) {
-                                if (el && el.local) {
-                                    return el.local;
-                                }
-                            });
-
-                            storedCoordinatesTrapArray = a.storedCoordinatesTraps;
-
-                            ////////// Action
-                            actionOnTravelFrame();
-                        });
-                    }
+              if (healthCheck < 15) {
+                smaugGet(["clothes"], function (act) {
+                  if (
+                    window.frames[locale.mainFrame].frames[
+                      locale.armyFrame
+                    ].document.querySelectorAll(
+                      "[src='" + act.clothes.svitokHealthPic + "']"
+                    )[0]
+                  ) {
+                    window.frames[locale.mainFrame].frames[
+                      locale.armyFrame
+                    ].document
+                      .querySelectorAll(
+                        "[src='" + act.clothes.svitokHealthPic + "']"
+                      )[0]
+                      .click();
+                  }
                 });
+              }
             }
-            loop();
-        }, rand);
-    }());
 
-    (function loop() {
-        var rand = Math.random() * (18000 - 10000) + 10000;
-        setTimeout(function () {
-            if (getArmyFrame()) {
-                smaugGet('action', function (act) {
-                    if (act.action == "go") {
-                        var randArmy = Math.floor(Math.random() * 3);
-                        var unit = getUnitNode(getArmyFrame(), locale.army[randArmy]);
-
-                        if (unit) {
-                            if (getArmyFrame()){
-                                unit.click();
-                            }
-                            setTimeout(function(){
-                                if (getArmyFrame()){
-                                    getArmyFrame().location.reload();
-                                    getCombatField().location.reload();
-                                }
-                            }, 3000);
-                        }
-
-                        if (getHealthNode(getArmyFrame())) {
-                            var healthCheck = parseInt(getHealthNode(getArmyFrame()).getAttribute("style").match(/\d+/)[0]);
-                            chrome.runtime.sendMessage({action: "combat", health: healthCheck});
-
-                            if (healthCheck < 15) {
-                                smaugGet(['clothes'], function (act) {
-                                    if (window.frames[locale.mainFrame].frames[locale.armyFrame].document.querySelectorAll("[src='" + act.clothes.svitokHealthPic + "']")[0]) {
-                                        window.frames[locale.mainFrame].frames[locale.armyFrame].document.querySelectorAll("[src='" + act.clothes.svitokHealthPic + "']")[0].click()
-                                    }
-                                });
-                            }
-                        }
-
-                        if (getExitLink(getCombatField())) {
-                            checkInitDaily("creature", 1, function () {
-                                getExitLink(getCombatField()).click();
-                            });
-                        }
-                    }
-                });
-
+            if (getExitLink(getCombatField())) {
+              checkInitDaily("creature", 1, function () {
+                getExitLink(getCombatField()).click();
+              });
             }
-            loop();
-        }, rand);
-    }());
-
+          }
+        });
+      }
+      loop();
+    }, rand);
+  })();
 }, 5000);
 
-
 setInterval(function () {
-    if (getTravelFrame() && getImageOfLocation(getTravelFrame()) !== "http://fantasyland.ru/images/places/Global/arena.jpg") {
-        smaugGet(['action', 'clothes', 'travelClothesEnable'], function (act) {
-            if (act.action == "go" && !act.travelClothesEnable) {
-                smaugSendRequest(locale.wearLink + act.clothes.travel, function () {
-                    smaugSet({
-                        'travelClothesEnable': true
-                    }, function () {
-                        console.log("Travel Clothes has been enabled");
-                    });
-                });
-
+  if (
+    getTravelFrame() &&
+    getImageOfLocation(getTravelFrame()) !==
+      "http://fantasyland.ru/images/places/Global/arena.jpg"
+  ) {
+    smaugGet(["action", "clothes", "travelClothesEnable"], function (act) {
+      if (act.action == "go" && !act.travelClothesEnable) {
+        smaugSendRequest(locale.wearLink + act.clothes.travel, function () {
+          smaugSet(
+            {
+              travelClothesEnable: true,
+            },
+            function () {
+              console.log("Travel Clothes has been enabled");
             }
+          );
         });
-    }
+      }
+    });
+  }
 }, 1000);
 
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        if (request.action === "reload") {
-            smaugGet(['action', 'clothes'], function (act) {
-                if (act.action == "go" && act.clothes.reload == true) {
-                    location.reload();
-                }
-            });
-
-
-        } else if (request.action === "drinkBeverage") {
-
-            if (getTravelFrame()  && getImageOfLocation(getTravelFrame()) !== "http://fantasyland.ru/images/places/Global/arena.jpg") {
-                smaugGet(['clothes', 'action'], function (act) {
-                    if (act.action == "go" && getTravelFrame()) {
-                        if (act.clothes.beverage1) {
-                            smaugSendRequest(locale.drinkLink + act.clothes.beverage1, function () {
-                                //console.log("Beverage has been drinked");
-                            });
-                        }
-                        if (act.clothes.beverage2) {
-                            smaugSendRequest(locale.drinkLink + act.clothes.beverage2, function () {
-                                //console.log("Beverage has been drinked");
-                            });
-                        }
-                        if (act.clothes.beverage3) {
-                            smaugSendRequest(locale.drinkLink + act.clothes.beverage3, function () {
-                                //console.log("Beverage has been drinked");
-                            });
-                        }
-                        if (act.clothes.beverage4) {
-                            smaugSendRequest(locale.drinkLink + act.clothes.beverage4, function () {
-                                //console.log("Beverage has been drinked");
-                            });
-                        }
-
-                    }
-                });
-            }
-
-
-        } else if (request.action === "useHealth") {
-            smaugGet(['clothes', 'action'], function (act) {
-
-                if (act.action == "go" && act.clothes.svitokHealth && getTravelFrame()) {
-                    smaugSendRequest(locale.drinkLink + act.clothes.svitokHealth, function () {
-                    });
-                    smaugSendRequest(locale.drinkLink + act.clothes.svitokHealth, function () {
-                    });
-                    smaugSendRequest(locale.drinkLink + act.clothes.svitokHealth, function () {
-                    });
-                    smaugSendRequest(locale.drinkLink + act.clothes.svitokHealth, function () {
-                    });
-                    smaugSendRequest(locale.drinkLink + act.clothes.svitokHealth, function () {
-                    });
-                    smaugSendRequest(locale.drinkLink + act.clothes.svitokHealth, function () {
-                    });
-                    smaugSendRequest(locale.drinkLink + act.clothes.svitokHealth, function () {
-                    });
-                    smaugSendRequest(locale.drinkLink + act.clothes.svitokHealth, function () {
-                    });
-                }
-
-            });
-        } else if (request.action === "initDelay") {
-            smaugSet({
-                'exit': true
-            }, function () {
-                console.log("If not in combat - Quit");
-            });
-        } else if (request.action === "shuffleArrows") {
-            if (getTravelFrame() && getImageOfLocation(getTravelFrame()) !== "http://fantasyland.ru/images/places/Global/arena.jpg") {
-                shuffle(arrowsArray);
-                storedCoordinates = [];
-                chrome.storage.local.remove("coordinates");
-            }
-        }
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.action === "reload") {
+    smaugGet(["action", "clothes"], function (act) {
+      if (act.action == "go" && act.clothes.reload == true) {
+        location.reload();
+      }
     });
+  } else if (request.action === "drinkBeverage") {
+    if (
+      getTravelFrame() &&
+      getImageOfLocation(getTravelFrame()) !==
+        "http://fantasyland.ru/images/places/Global/arena.jpg"
+    ) {
+      smaugGet(["clothes", "action"], function (act) {
+        if (act.action == "go" && getTravelFrame()) {
+          if (act.clothes.beverage1) {
+            smaugSendRequest(
+              locale.drinkLink + act.clothes.beverage1,
+              function () {
+                //console.log("Beverage has been drinked");
+              }
+            );
+          }
+          if (act.clothes.beverage2) {
+            smaugSendRequest(
+              locale.drinkLink + act.clothes.beverage2,
+              function () {
+                //console.log("Beverage has been drinked");
+              }
+            );
+          }
+          if (act.clothes.beverage3) {
+            smaugSendRequest(
+              locale.drinkLink + act.clothes.beverage3,
+              function () {
+                //console.log("Beverage has been drinked");
+              }
+            );
+          }
+          if (act.clothes.beverage4) {
+            smaugSendRequest(
+              locale.drinkLink + act.clothes.beverage4,
+              function () {
+                //console.log("Beverage has been drinked");
+              }
+            );
+          }
+        }
+      });
+    }
+  } else if (request.action === "useHealth") {
+    smaugGet(["clothes", "action"], function (act) {
+      if (act.action == "go" && act.clothes.svitokHealth && getTravelFrame()) {
+        smaugSendRequest(
+          locale.drinkLink + act.clothes.svitokHealth,
+          function () {}
+        );
+        smaugSendRequest(
+          locale.drinkLink + act.clothes.svitokHealth,
+          function () {}
+        );
+        smaugSendRequest(
+          locale.drinkLink + act.clothes.svitokHealth,
+          function () {}
+        );
+        smaugSendRequest(
+          locale.drinkLink + act.clothes.svitokHealth,
+          function () {}
+        );
+        smaugSendRequest(
+          locale.drinkLink + act.clothes.svitokHealth,
+          function () {}
+        );
+        smaugSendRequest(
+          locale.drinkLink + act.clothes.svitokHealth,
+          function () {}
+        );
+        smaugSendRequest(
+          locale.drinkLink + act.clothes.svitokHealth,
+          function () {}
+        );
+        smaugSendRequest(
+          locale.drinkLink + act.clothes.svitokHealth,
+          function () {}
+        );
+      }
+    });
+  } else if (request.action === "initDelay") {
+    smaugSet(
+      {
+        exit: true,
+      },
+      function () {
+        console.log("If not in combat - Quit");
+      }
+    );
+  } else if (request.action === "shuffleArrows") {
+    if (
+      getTravelFrame() &&
+      getImageOfLocation(getTravelFrame()) !==
+        "http://fantasyland.ru/images/places/Global/arena.jpg"
+    ) {
+      shuffle(arrowsArray);
+      storedCoordinates = [];
+      chrome.storage.local.remove("coordinates");
+    }
+  }
+});
--- functions/node.js
+++ functions/node.js
@@ -1,90 +1,99 @@
-var getHealthTab = function(){
-    return window.frames["loc"].frames["show_info"].document.querySelector(locale.healthTab)
+var getHealthTab = function () {
+  return window.frames["loc"].frames["show_info"].document.querySelector(
+    locale.healthTab
+  );
 };
 
-var getTravelFrame = function() {
-    return window.frames[locale.mainFrame].frames[locale.noCombatFrame];
+var getTravelFrame = function () {
+  return window.frames[locale.mainFrame].frames[locale.noCombatFrame];
 };
 
-var getCoordinateNode = function(travelFrame){
-    return travelFrame.document.getElementById(locale.coordinatesText);
+var getCoordinateNode = function (travelFrame) {
+  return travelFrame.document.getElementById(locale.coordinatesText);
 };
 
-var getMobNode = function(travelFrame){
-    return travelFrame.document.getElementById("plrs").querySelectorAll(locale.attack);
+var getMobNode = function (travelFrame) {
+  return travelFrame.document
+    .getElementById("plrs")
+    .querySelectorAll(locale.attack);
 };
 
-var getCharNode = function(travelFrame){
-    return travelFrame.document.getElementById("plrs").querySelectorAll(locale.player);
+var getCharNode = function (travelFrame) {
+  return travelFrame.document
+    .getElementById("plrs")
+    .querySelectorAll(locale.player);
 };
 
-var getImageOfLocation = function(travelFrame){
-    if (travelFrame && travelFrame.document.querySelector(".cssLocImage")) {
-        return travelFrame.document.querySelector(".cssLocImage").src;
-    } else {
-        return false;
-    }
-
+var getImageOfLocation = function (travelFrame) {
+  if (travelFrame && travelFrame.document.querySelector(".cssLocImage")) {
+    return travelFrame.document.querySelector(".cssLocImage").src;
+  } else {
+    return false;
+  }
 };
 
-var getItemNode = function(travelFrame){
-    return travelFrame.document.getElementById("picks");
+var getItemNode = function (travelFrame) {
+  return travelFrame.document.getElementById("picks");
 };
 
-var getInputCaptcha = function(travelFrame){
-    return travelFrame.document.getElementById("value");
+var getInputCaptcha = function (travelFrame) {
+  return travelFrame.document.getElementById("value");
 };
 
-var getSpeedNode = function(travelFrame) {
-    return travelFrame.document.getElementById("hru");
+var getSpeedNode = function (travelFrame) {
+  return travelFrame.document.getElementById("hru");
 };
 
-var getArrowNode = function(travelFrame, arrow) {
-    return travelFrame.document.getElementById(locale.coordinates[arrow]).firstElementChild;
+var getArrowNode = function (travelFrame, arrow) {
+  return travelFrame.document.getElementById(locale.coordinates[arrow])
+    .firstElementChild;
 };
 
 var getHealthBarWidth = function () {
-    var getHealthBarNode = window.frames[locale.mainFrame].frames[locale.showInfoFrame].document.querySelector(locale.healthLine);
-    if (getHealthBarNode) {
-        return getHealthBarNode.style.width;
-    } else {
-        return false;
-        //window.frames[locale.mainFrame].frames[locale.showInfoFrame].document.querySelector(locale.healthTab).click();
-    }
+  var getHealthBarNode = window.frames[locale.mainFrame].frames[
+    locale.showInfoFrame
+  ].document.querySelector(locale.healthLine);
+  if (getHealthBarNode) {
+    return getHealthBarNode.style.width;
+  } else {
+    return false;
+    //window.frames[locale.mainFrame].frames[locale.showInfoFrame].document.querySelector(locale.healthTab).click();
+  }
 };
 
-var getArmyFrame = function() {
-    return window.frames[locale.mainFrame].frames[locale.armyFrame];
+var getArmyFrame = function () {
+  return window.frames[locale.mainFrame].frames[locale.armyFrame];
 };
 
-var getHealthNode = function(combatFrame){
+var getHealthNode = function (combatFrame) {
   return combatFrame.document.querySelector("#hp_col44322");
 };
 
-var getExpNode = function() {
-    if (window.frames[locale.mainFrame].frames[locale.showInfoFrame]) {
-        return window.frames[locale.mainFrame].frames[locale.showInfoFrame].document.querySelector(locale.expLine);
-    } else {
-        return false;
-    }
-
+var getExpNode = function () {
+  if (window.frames[locale.mainFrame].frames[locale.showInfoFrame]) {
+    return window.frames[locale.mainFrame].frames[
+      locale.showInfoFrame
+    ].document.querySelector(locale.expLine);
+  } else {
+    return false;
+  }
 };
 
-var getCombatField = function() {
-    return window.frames[locale.mainFrame].frames[locale.combatFrame];
+var getCombatField = function () {
+  return window.frames[locale.mainFrame].frames[locale.combatFrame];
 };
 
-var getUnitNode = function(armyFrame, armyType){
-    var armySelector = "#" + armyType + " .ArmyShow .cp[onclick][valign=middle]";
-    var chooseArmy = armyFrame.document.querySelectorAll(armySelector);
-    return  chooseArmy[chooseArmy.length - 1];
+var getUnitNode = function (armyFrame, armyType) {
+  var armySelector = "#" + armyType + " .ArmyShow td > div";
+  var chooseArmy = armyFrame.document.querySelectorAll(armySelector);
+  return chooseArmy[chooseArmy.length - 1];
 };
 
 //var getUnitNode = function(armyFrame){
 //    return armyFrame.document.querySelector("");
 //};
 
-var getExitLink = function(combatFrame){
-    var laa = combatFrame.document.querySelector("#la a");
-    return laa;
+var getExitLink = function (combatFrame) {
+  var laa = combatFrame.document.querySelector("#la a");
+  return laa;
 };
